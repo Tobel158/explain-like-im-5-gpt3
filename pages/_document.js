4d3343a14e6466849c22e91a858165742d444650
Tobel Atnafu <tobelwubshet@gmail.com>
2022-11-29 21:55:00 -0500

    add meta

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Explain Like I'm 5 🐣" key="title"/>
+        <meta property="og:description" content="Make Any Topic Fun and Easy to Learn!" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://raw.githubusercontent.com/Tobel158/explain-like-im-5-gpt3/main/assets/birdie.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
